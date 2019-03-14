4900cf4d72a34df976f186af760b46306bf7a6c8
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2019-03-14 14:47:31 +0100

    Get gsheet data.

--- real_time_reporting/app.R
+++ real_time_reporting/app.R
@@ -1,39 +1,29 @@
 library(shiny)
+library(shinydashboard)
+library(googlesheets)
+suppressPackageStartupMessages(library(tidyverse))
+
+url <- read_lines("real_time_reporting/gs_url.txt")
+
+ss <- gs_url(url, visibility = "public")
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody(
+    fluidRow(
+      box(plotOutput("plot", height = 250)))
+  )
 )
-
+  
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  
+  output$plot <- renderPlot({
+    data <- 
+  }) 
+  
 }
 
 # Run the application 
