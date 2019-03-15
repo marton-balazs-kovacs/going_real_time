49cb8f63a5020ce5e2b59b1a8f88c780ae0615ee
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2019-03-15 18:45:27 +0100

    ObserveEvent expression is running even if there is no new data in the gsheet. If I add NULL as event evaluation it still runs seemingly as it drops warnings but do not show the results. WAT?

--- microbenchmark_real_time_reporting.R
+++ microbenchmark_real_time_reporting.R
@@ -4,7 +4,10 @@ url <- read_lines("real_time_reporting/gs_url.txt")
 
 ss <- gs_url(url, visibility = "public")
 
-data_or_registration <- microbenchmark(gs_url(url, visibility = "public"),
+read_or_registration <- microbenchmark(gs_url(url, visibility = "public"),
                                        gs_read(ss))
 
-
+gs_reads <- microbenchmark(gs_read(ss),
+                           gs_read_csv(ss),
+                           gs_read_cellfeed(ss),
+                           gs_read_listfeed(ss))
--- real_time_reporting/app.R
+++ real_time_reporting/app.R
@@ -37,50 +37,57 @@ server <- function(input, output) {
   
   refresh_time = 10000
   
-  observe({
+  form_data <- reactiveValues()
+  
+  read_data <- reactive({
     
     invalidateLater(refresh_time)
 
-    form_data <- gs_read(ss)
-    
-    form_data <- form_data %>% 
+    temp <- gs_read(ss) %>% 
       rename_all(funs(beg2char(., char = ".")))
     
-    output$scatter_plot <- renderPlot({
-      form_data %>%
+    temp
+  })
+  
+  observeEvent({!identical(form_data,read_data())},{
+    
+    form_data <- read_data()
+  
+  output$scatter_plot <- renderPlot({
+    form_data %>%
       ggplot() +
-        aes(x = Q2a,
-            y = Q0) +
-        geom_point() +
-        geom_smooth(method = "lm") +
-        labs(y = "Életkor",
-             x = "Adatgyűjtés automatizálásának várható ideje") +
-        theme_minimal()
-    }) 
+      aes(x = Q2a,
+          y = Q0) +
+      geom_point() +
+      geom_smooth(method = "lm") +
+      labs(y = "Életkor",
+           x = "Adatgyűjtés automatizálásának várható ideje") +
+      theme_minimal()
+  }) 
+  
+  output$cor <- renderTable(colnames = F, {
+    cor_test <- cor.test(form_data$Q0, form_data$Q2a, method = "spearman")
     
-    output$cor <- renderTable(colnames = F, {
-      cor_test <- cor.test(form_data$Q0, form_data$Q2a, method = "spearman")
-      
-      tibble(c("Spearman rho korrelációs együttható:", cor_test$estimate),
-             c("Megfigyelések száma:", cor_test$statistic))
-    })
-    
-    output$bar_plot <- renderPlot({
-      form_data %>% 
-        group_by(Q1) %>% 
-        summarise(mean = round(mean(Q0, na.rm = T), 2),
-                  sd = round(sd(Q0, na.rm = T), 2),
-                  n = n(),
-                  se = sd / sqrt(n)) %>% 
-        ggplot() +
-        aes(x = Q1, y = mean) +
-        geom_bar(stat = "identity") +
-        geom_errorbar(aes(ymin = mean - se,
-                          ymax = mean + se),
-                      position = "dodge") +
-        labs(x = "Lehetséges lesz automatizálni a tudományos folyamatot?",
-             y = "Kor") +
-        theme_minimal()})
+    tibble(c("Spearman rho korrelációs együttható:", cor_test$estimate),
+           c("Megfigyelések száma:", cor_test$statistic))
+  })
+  
+  output$bar_plot <- renderPlot({
+    form_data %>% 
+      group_by(Q1) %>% 
+      summarise(mean = round(mean(Q0, na.rm = T), 2),
+                sd = round(sd(Q0, na.rm = T), 2),
+                n = n(),
+                se = sd / sqrt(n)) %>% 
+      ggplot() +
+      aes(x = Q1, y = mean) +
+      geom_bar(stat = "identity") +
+      geom_errorbar(aes(ymin = mean - se,
+                        ymax = mean + se),
+                    position = "dodge") +
+      labs(x = "Lehetséges lesz automatizálni a tudományos folyamatot?",
+           y = "Kor") +
+      theme_minimal()})
   })
   }
 
