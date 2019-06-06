06da4a8bf37044b98d2c9a8b46e2df6ac8105f4f
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2019-06-06 16:10:13 +0200

    Commented

--- real_time_reporting/app.R
+++ real_time_reporting/app.R
@@ -1,18 +1,21 @@
+# Loading packages
 library(shiny)
 library(shinydashboard)
 library(googlesheets)
 suppressPackageStartupMessages(library(tidyverse))
 library(BayesFactor)
-library(shinyjs)
 
 # Define UI parts
-header <- dashboardHeader(title = "ISE 2019",
+## Define header
+header <- dashboardHeader(title = "Real Time Data Analysis",
                           dropdownMenuOutput("bf_warning"))
 
+## Define sidebar
 sidebar <- dashboardSidebar(id = "", sidebarMenu(
   menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
   menuItem("Bayes factor", tabName = "bayesfactor", icon = icon("th"))))
 
+## Define app body
 body <- dashboardBody(
   tabItems(
     
@@ -35,63 +38,88 @@ body <- dashboardBody(
                              height = 250))
             )
     ),
+    
+    # Second tab content
     tabItem(tabName = "bayesfactor",
             icon = icon("cog", lib = "glyphicon"),
             fluidRow(
-              box(title = "Bayes factor ábra",
+              box(title = "Bayes factor plot",
                   solidHeader = T,
+                  height = "auto",
+                  width = "auto",
                   plotOutput("bf",
-                             height = 250))
+                             height = 300,
+                             width = 600))
             )
     )
     ))
 
-# Define UI
+# Define UI with combining UI parts
 ui <- dashboardPage(header,
                     sidebar,
                     body)
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output) {
   
-  url <- "https://docs.google.com/spreadsheets/d/1ZL55BCrqqNNmSWr01g5ibL1yLNc-tfuk5IbKYlrnZu4/edit?usp=sharing"
+  # Read in the url of the google spreadsheet that contains the output of your google form
+  url <- read_lines("gs_url.txt")
   
+  # Create connection through the url with the spreadsheet
   ss <- gs_url(url, visibility = "public")
   
-  refresh_time = 10000
+  # Set the time interval for the refresh of the app (in milliseconds)
+  refresh_time <- 10000
   
-
+  # Create an empty reactive value for storing the output values
   values <- reactiveValues()
+  
+  # Set the default trigger value
   values$trigger <- NULL
+  
+  # Create an empty dataframe for storing the data
   values$form_data <- tibble()
+  
+  # Create a dataframe for storing the results of the Bayes factor calculation
   values$bf_data <- tibble(BF = numeric(0),
                            n_participant = integer(0))
   
+  # Create a reactive expression for reading in the data from the spreadsheet
   read_data <- reactive({
     
+    # Run the reactive expression after the time specified in [refresh_time]
     invalidateLater(refresh_time)
     
+    # Read in data and transform variable names
     temp <- gs_read(ss) %>% 
-      rename_all(~ str_to_lower(.)) %>% 
-      rename_at(vars(matches("[.]")), ~ str_extract_all(., "^([^.])+(?=\\.)"))
-    
+      rename_all(~ str_to_lower(.)) %>% # Convert variable names to lower case  
+      rename_at(vars(matches("[.]")),
+                ~ str_extract(., "^([^.])+(?=\\.)")) # Save the identifiers of the question as variable names
+  
     temp
   })
   
-
+  # Create an expression that triggers the analysis if there are new responses to the form
   observe({
     
     invalidateLater(refresh_time)
     
-    if(! identical(values$form_data, read_data())){
+    if(!identical(values$form_data, read_data())){
+      
       values$trigger <-1
       values$form_data <- read_data()
-    }else{values$trigger <- NULL}
+      
+    }else{
+      
+      values$trigger <- NULL}
   })
   
+  # Create an expression for plotting the results of the analysis that is triggered by new responses
   observeEvent(values$trigger,{
- 
+    
+    # Create a scatterplot
     output$scatter_plot <- renderPlot({
+      
       values$form_data %>%
         ggplot() +
         aes(x = q2a,
@@ -103,6 +131,7 @@ server <- function(input, output) {
         theme_minimal()
     }) 
     
+    # Create a table that shows correlation between variables
     output$cor <- renderTable(colnames = F, {
       cor_test <- cor.test(values$form_data$q0, values$form_data$q2a, method = "spearman")
       n_answers <- values$form_data %>% count()
@@ -111,6 +140,7 @@ server <- function(input, output) {
              c("Sample size:", n_answers))
     })
     
+    # Create a barplot
     output$bar_plot <- renderPlot({
       values$form_data %>% 
         group_by(q1) %>% 
@@ -124,25 +154,31 @@ server <- function(input, output) {
         geom_errorbar(aes(ymin = mean - se,
                           ymax = mean + se),
                       position = "dodge") +
-        labs(x = "I will be possible to fully automatize the research process",
+        labs(x = "I believe that it will be possible to fully automatize the research process",
              y = "Age") +
         theme_minimal()})
     
   })
   
+  # Create An expression for the Baye factor analysis
   observeEvent(values$trigger,{
     
+    # Save data for the BF analysis
     bf_temp <- values$form_data %>% 
       filter(!is.na(q2a)) %>% 
       mutate(q1 = as.factor(q1))
     
+    # Count the number of observations
     bf_temp_n <- bf_temp %>%
       count() %>% 
       pull(n)
     
+    # Set the minimum number of participants required for the analyis
     bf_min_participant = 15
     
+    
     if(bf_temp_n > bf_min_participant){
+      
       output$bf_warning <- NULL
       if(nrow(values$bf_data) == 0){
         
@@ -155,6 +191,7 @@ server <- function(input, output) {
           values$bf_data <- bind_rows(values$bf_data, new_line)}
         
         }else{
+          
         new_participant_n <- max(values$bf_data$n_participant)
         
         for(i in new_participant_n:bf_temp_n){
@@ -166,8 +203,16 @@ server <- function(input, output) {
       
 
     
-    # Plot
+    # Create the plot for the BF analysis
     x_limit_max <- max(values$bf_data$n_participant) * 1.5
+    
+    y_limit_max <- if_else(max(values$bf_data$BF) > log(40),
+                           max(values$bf_data$BF),
+                           log(40))
+    
+    y_limit_min <- if_else(min(values$bf_data$BF) < -log(40),
+                           min(values$bf_data$BF),
+                           -log(40))
     
     output$bf <- renderPlot({
       values$bf_data %>% 
@@ -179,7 +224,7 @@ server <- function(input, output) {
         scale_y_continuous(breaks = c(c(-log(c(30, 10, 3)), 0, log(c(3, 10, 30)))),
                            labels = c("-log(30)", "-log(10)", "-log(3)", "log(1)", "log(3)", "log(10)", "log(30)")) +
         scale_x_continuous(limits = c(bf_min_participant, x_limit_max)) +
-        coord_cartesian(ylim=c(-log(40),log(40))) +
+        coord_cartesian(ylim=c(y_limit_min, y_limit_max)) +
         theme_minimal() +
         geom_hline(yintercept=c(c(-log(c(30, 10, 3)), log(c(3, 10, 30)))), linetype="dotted", color="darkgrey") +
         geom_hline(yintercept=log(1), linetype="dashed", color="darkgreen") +
@@ -195,13 +240,15 @@ server <- function(input, output) {
       })
     
     }else{
+      
       output$bf <- NULL
       
-    output$bf_warning <- renderMenu({
-      dropdownMenu(type = "notifications", badgeStatus = "warning",
-                   notificationItem(text = "Sample size is not enough for BF analysis.",
-                                    icon = icon("ok", lib = "glyphicon"),
-                                    status = "danger"))
+      # Create a notification icon if the sample size is not enough for BF analysis
+      output$bf_warning <- renderMenu({
+        dropdownMenu(type = "notifications", badgeStatus = "warning",
+                     notificationItem(text = "Sample size is not enough for BF analysis.",
+                                      icon = icon("ok", lib = "glyphicon"),
+                                      status = "danger"))
   })
 }
   })
